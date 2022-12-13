60dfcdd38b025a42b15e61b4e1a427fc90cf9f1e
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2022-12-13 23:37:54 +0100

    Commit template

---
+++ 2022/template/src.swift
@@ -0,0 +1,17 @@
+import AdventOfCode
+import Algorithms
+import Collections
+
+@main
+public struct Day {
+    public static func main() {
+        solve().print()
+    }
+
+    public static func solve() -> Solution<Int, Int> {
+        var n1 = 0
+        var n2 = 0
+
+        return Solution(n1: n1, n2: n2)
+    }
+}
