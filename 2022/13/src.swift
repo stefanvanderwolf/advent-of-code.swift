b1451b064ccd94c344bbd48dac95da6be8d5532d
Stefan van der Wolf <stefan.van.der.wolf@persgroep.net>
2022-12-13 23:57:02 +0100

    Reuse code

--- 2022/13/src.swift
+++ 2022/13/src.swift
@@ -2,31 +2,27 @@ import AdventOfCode
 import Algorithms
 import Foundation
 
-struct Comparator: SortComparator {
-    var order: SortOrder = .forward
-
-    func compare(_ lhs: Any, _ rhs: Any) -> ComparisonResult {
-        if let lhs = lhs as? Int, let rhs = rhs as? Int {
-            return lhs.compare(rhs)
-        }
-
-        guard let lhs = lhs as? [Any], let rhs = rhs as? [Any] else {
-            if let lhs = lhs as? Int {
-                return compare([lhs], rhs)
-            } else {
-                return compare(lhs, [rhs])
-            }
-        }
-
-        for i in 0 ..< min(lhs.count, rhs.count) {
-            let x = compare(lhs[i], rhs[i])
-            if x != .orderedSame {
-                return x
-            }
-        }
-
-        return lhs.count.compare(rhs.count)
+func cmp(_ lhs: Any, _ rhs: Any) -> Int {
+    if let lhs = lhs as? Int, let rhs = rhs as? Int {
+        return lhs.cmp(rhs)
     }
+
+    guard let lhs = lhs as? [Any], let rhs = rhs as? [Any] else {
+        if let lhs = lhs as? Int {
+            return cmp([lhs], rhs)
+        } else {
+            return cmp(lhs, [rhs])
+        }
+    }
+
+    for i in 0 ..< min(lhs.count, rhs.count) {
+        let x = cmp(lhs[i], rhs[i])
+        if x != 0 {
+            return x
+        }
+    }
+
+    return lhs.count.cmp(rhs.count)
 }
 
 @main
@@ -42,12 +38,10 @@ public struct Day {
                 try! JSONSerialization.jsonObject(with: $0.data(using: .utf8)!)
             }
 
-        let comparator = Comparator()
-
         let n1 = input
             .chunks(ofCount: 2)
             .enumerated()
-            .filter { comparator.compare($0.element.first!, $0.element.last!) == .orderedAscending }
+            .filter { cmp($0.element.first!, $0.element.last!) == -1 }
             .reduce(0) { $0 + $1.offset + 1}
 
         let dividers: [Any] = [
@@ -56,19 +50,16 @@ public struct Day {
         ]
 
         let n2 = (input + dividers)
-            .sorted(using: comparator)
+            .sorted { cmp($0, $1) < 0 }
             .enumerated()
             .filter { e -> Bool in
                 dividers.contains {
-                    comparator.compare($0, e.element) == .orderedSame
+                    cmp($0, e.element) == 0
                 }
             }
             .map { $0.offset + 1 }
             .product
 
-        return Solution(
-            n1: n1,
-            n2: n2
-        )
+        return Solution(n1: n1, n2: n2)
     }
 }
